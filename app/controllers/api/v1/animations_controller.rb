e5d366d32efaeb6ee8fffe2a929234dd66b0215a
kenneth-yu <kyu22@buffalo.edu>
2019-02-20 15:21:05 -0500

    bug fixes

--- app/controllers/api/v1/animations_controller.rb
+++ app/controllers/api/v1/animations_controller.rb
@@ -1,13 +1,5 @@
 class Api::V1::AnimationsController < ApplicationController
-<<<<<<< HEAD
-  before_action :find_animation, only: [:update]
-=======
-<<<<<<< HEAD
   before_action :find_animation, only: [:show, :update]
-=======
-  before_action :find_animation, only: [:update]
->>>>>>> master
->>>>>>> 14d6cb79119096c4bf4905ffde8ed40ab571d9b8
   def index
     @animations = Animation.all
     render json: @animations
